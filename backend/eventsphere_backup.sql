02dbde2200c4f0c1c9ce5acada665842610f9773
stutinainwal14 <nainwalstuti1402@gmail.com>
2025-06-11 09:47:02 +0000

    updated database structure

--- backend/eventsphere_backup.sql
+++ backend/eventsphere_backup.sql
@@ -16,6 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `BlacklistedTokens`
+--
+
+DROP TABLE IF EXISTS `BlacklistedTokens`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `BlacklistedTokens` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `token` text NOT NULL,
+  `blacklisted_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `BlacklistedTokens`
+--
+
+LOCK TABLES `BlacklistedTokens` WRITE;
+/*!40000 ALTER TABLE `BlacklistedTokens` DISABLE KEYS */;
+INSERT INTO `BlacklistedTokens` VALUES (1,'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MzgsInJvbGUiOiJhZG1pbiIsImlhdCI6MTc0OTYzMzE5NiwiZXhwIjoxNzUwMjM3OTk2fQ.wZHpQO0DNX7zk-z-1JiOYDuXipy0NR34FIMr5GKMboY','2025-06-11 09:13:40');
+/*!40000 ALTER TABLE `BlacklistedTokens` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `SavedEvents`
 --
 
@@ -32,7 +57,7 @@ CREATE TABLE `SavedEvents` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `user_id` (`user_id`,`event_id`),
   CONSTRAINT `SavedEvents_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`user_id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -62,7 +87,7 @@ CREATE TABLE `Users` (
   `isTwoFactorEnabled` tinyint(1) DEFAULT '0',
   `avatar` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -71,7 +96,7 @@ CREATE TABLE `Users` (
 
 LOCK TABLES `Users` WRITE;
 /*!40000 ALTER TABLE `Users` DISABLE KEYS */;
-INSERT INTO `Users` VALUES (32,'Stuti Nainwal','nainwalstuti1402@gmail.com','$2b$10$6WqHoQopMUXTFRMTsYZe6OZWKJtLSjiQ3ogqEUr5nkQgiJkW3KuBO','user','{}',NULL,0,NULL),(34,NULL,'test@123.com','$2b$10$mGe9ZAT8j25wuv6rY1kpxubI0TPRbj6IUQ6I7j40FQ2RRsB4P6wvK','user','{\"music\": \"true\"}',NULL,0,NULL),(35,NULL,'stuti@123.com','$2b$10$BFyR3LJKENbDh0nYsZPHCOjHShMoYkIT4UjKNXx2AKNCcbBsMNhTW','user','{\"music\": \"true\"}',NULL,0,NULL),(36,NULL,'stuti@gmail.com','$2b$12$XiaQ97.ijIlg3aRxodK2.O5fpxTyzaWZI1uo.weAtgUG2UoCBhFDu','admin','{\"theme\": \"dark\"}',NULL,0,'/uploads/avatar-1749484389606-97223258.jpg'),(37,'Stuti Nainwal','nainwalstuti14023@gmail.com','$2b$12$BLkekg3yvQrwH8QKPp9h/OKGgEwmgu2zrpBIqhBwOCzOuv.dIMPvi','user','{}','FR4UI3LJERUWWNTLF4VH2USHPM7WW6TEJFCDGVDJPBCD6QJOHNDQ',1,NULL);
+INSERT INTO `Users` VALUES (32,'Stuti Nainwal stuti','nainwalstuti12@gmail.com','$2b$10$6WqHoQopMUXTFRMTsYZe6OZWKJtLSjiQ3ogqEUr5nkQgiJkW3KuBO','user','{}',NULL,0,NULL),(34,NULL,'test@123.com','$2b$10$mGe9ZAT8j25wuv6rY1kpxubI0TPRbj6IUQ6I7j40FQ2RRsB4P6wvK','user','{\"music\": \"true\"}',NULL,0,NULL),(35,NULL,'stuti@123.com','$2b$10$BFyR3LJKENbDh0nYsZPHCOjHShMoYkIT4UjKNXx2AKNCcbBsMNhTW','user','{\"music\": \"true\"}',NULL,0,NULL),(36,NULL,'stuti@gmail.com','$2b$12$XiaQ97.ijIlg3aRxodK2.O5fpxTyzaWZI1uo.weAtgUG2UoCBhFDu','admin','{\"theme\": \"dark\"}',NULL,0,'/uploads/avatar-1749484389606-97223258.jpg'),(37,'Stuti Nainwal','nainwalstuti14023@gmail.com','$2b$12$BLkekg3yvQrwH8QKPp9h/OKGgEwmgu2zrpBIqhBwOCzOuv.dIMPvi','user','{}','FR4UI3LJERUWWNTLF4VH2USHPM7WW6TEJFCDGVDJPBCD6QJOHNDQ',1,NULL),(38,NULL,'stutinainwal@gmail.com','$2b$12$tvMOsY11IXQWBA5Lzj/3ru.ReDxhlgQZO9tv7Vci/rlcvghGw1qvu','admin','{\"music\": \"true\"}',NULL,0,NULL),(39,'Stuti Nainwal','nainwalstuti140234@gmail.com','$2b$12$uByJi5j3STovirXj4xBBReWP1ZsHl.8CkQJTNniKplB/l4v636yFW','user','{}',NULL,0,NULL),(40,'admin','admin123@gmail.com','$2b$12$PhqvzhjEeeWwCSKOI8tYHO8ul9ErX0JWDHNHeBywS2kiUqAEkT2vq','admin','{}',NULL,0,NULL);
 /*!40000 ALTER TABLE `Users` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -109,4 +134,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-06-09 15:58:09
+-- Dump completed on 2025-06-11  9:46:50
